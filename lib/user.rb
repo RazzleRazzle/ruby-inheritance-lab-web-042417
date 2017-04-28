2b6cf44831121136a106e17783b2cb2391746c5b
Jake Musich <makejusich@gmail.com>
2017-04-28 10:33:15 -0400

    Done.

---
+++ lib/user.rb
@@ -0,0 +1,26 @@
+class User
+
+  attr_accessor :first, :last
+
+  def initialize()
+    @first = first
+    @last = last
+  end
+
+  def first_name=(first)
+    @first = first
+  end
+
+  def first_name
+    @first
+  end
+
+  def last_name=(last)
+    @last = last
+  end
+
+  def last_name
+    @last = last
+  end
+
+end
